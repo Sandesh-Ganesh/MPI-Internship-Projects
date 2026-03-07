365fd553ebf04686bd8ce6d7d491c483060f89c1
Sandesh-Ganesh <raosandesh70@gmail.com>
2026-03-07 23:34:34 +0530

    Add CostCenter model

---
+++ DNS Management System/backend/src/models/CostCenter.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize"
+import sequelize from "../config/database.js"
+
+const CostCenter = sequelize.define("CostCenter",
+    {
+        cost_center_id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        cost_center_name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        status:{
+            type:DataTypes.ENUM("ACTIVE","INACTIVE"),
+            allowNull:false,
+            defaultValue:"ACTIVE"
+        }
+    },
+    {
+        tableName:"cost_centers",
+        timestamps:false
+    }
+)
+
+export default CostCenter
