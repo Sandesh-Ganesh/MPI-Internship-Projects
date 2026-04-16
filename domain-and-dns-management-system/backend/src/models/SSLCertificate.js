3aa406fb0a691ecad82ab273fb60da3a06bba419
Sandesh-Ganesh <raosandesh70@gmail.com>
2026-04-16 23:13:07 +0530

    Add SSL Certificate model

---
+++ domain-and-dns-management-system/backend/src/models/SSLCertificate.js
@@ -0,0 +1,79 @@
+import { DataTypes } from "sequelize"
+import sequelize from "../config/database.js"
+
+const SSLCertificate = sequelize.define(
+  "SSLCertificate",
+  {
+    ssl_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    domain_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    ssl_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    cert_type: {
+      type: DataTypes.ENUM("DV","OV","EV"),
+      allowNull: false
+    },
+
+    validation_type: {
+      type: DataTypes.ENUM("DNS","EMAIL"),
+      allowNull: false
+    },
+
+    encryption_type: {
+      type: DataTypes.ENUM("RSA","ECC"),
+      allowNull: false
+    },
+
+    registered_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+
+    expiry_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+
+    vendor_id: {
+      type: DataTypes.INTEGER
+    },
+
+    control_panel_id: {
+      type: DataTypes.INTEGER
+    },
+
+    requested_by: {
+      type: DataTypes.INTEGER
+    },
+
+    approved_by: {
+      type: DataTypes.INTEGER
+    },
+
+    remarks: {
+      type: DataTypes.TEXT
+    },
+
+    status: {
+      type: DataTypes.ENUM("ACTIVE","EXPIRED","INACTIVE"),
+      defaultValue: "ACTIVE"
+    }
+  },
+  {
+    tableName: "ssl_certificates",
+    timestamps: true
+  }
+)
+
+export default SSLCertificate
