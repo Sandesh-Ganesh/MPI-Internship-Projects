d93da0aff098b19e1420efa035421bfc08093cc2
Sandesh-Ganesh <raosandesh70@gmail.com>
2026-05-06 11:24:41 +0530

    Modified Login and Signup pages

--- domain-and-dns-management-system/frontend/src/app/modules/auth/components/Login.tsx
+++ domain-and-dns-management-system/frontend/src/app/modules/auth/components/Login.tsx
@@ -1,12 +1,10 @@
 import {useState} from 'react'
 import * as Yup from 'yup'
 import clsx from 'clsx'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {useFormik} from 'formik'
-import {getUserByToken, login} from '../core/_requests'
-import {toAbsoluteUrl} from '../../../../_metronic/helpers'
+import {login} from '../core/_requests'
 import {useAuth} from '../core/Auth'
-import {useNavigate} from 'react-router-dom'
 
 const loginSchema = Yup.object().shape({
   email: Yup.string()
@@ -14,6 +12,7 @@ const loginSchema = Yup.object().shape({
     .min(3, 'Minimum 3 symbols')
     .max(50, 'Maximum 50 symbols')
     .required('Email is required'),
+
   password: Yup.string()
     .min(3, 'Minimum 3 symbols')
     .max(50, 'Maximum 50 symbols')
@@ -21,46 +20,44 @@ const loginSchema = Yup.object().shape({
 })
 
 const initialValues = {
-  email: 'admin@demo.com',
-  password: 'demo',
+  email: '',
+  password: '',
 }
-
-/*
-  Formik+YUP+Typescript:
-  https://jaredpalmer.com/formik/docs/tutorial#getfieldprops
-  https://medium.com/@maurice.de.beijer/yup-validation-and-typescript-and-formik-6c342578a20e
-*/
-
-
 
 export function Login() {
   const [loading, setLoading] = useState(false)
+
   const {saveAuth, setCurrentUser} = useAuth()
+
   const navigate = useNavigate()
 
   const formik = useFormik({
     initialValues,
     validationSchema: loginSchema,
+
     onSubmit: async (values, {setStatus, setSubmitting}) => {
       setLoading(true)
+
       try {
-          const response = await login(values.email, values.password)
+        const response = await login(values.email, values.password)
 
-          const data = response.data
+        const data = response.data
 
-          //  Save token
-          saveAuth({ api_token: data.token })
+        // Save token
+        saveAuth({api_token: data.token})
 
-          // Save user in context
-          setCurrentUser(data.user)
+        // Save user
+        setCurrentUser(data.user)
 
-          // Go to dashboard
-          navigate('/dashboard')
-
-        } catch (error) {
+        // Redirect
+        navigate('/dashboard')
+      } catch (error) {
         console.error(error)
+
         saveAuth(undefined)
-        setStatus('The login details are incorrect')
+
+        setStatus('Invalid email or password')
+
         setSubmitting(false)
         setLoading(false)
       }
@@ -74,119 +71,77 @@ export function Login() {
       noValidate
       id='kt_login_signin_form'
     >
-      {/* begin::Heading */}
+      {/* Heading */}
       <div className='text-center mb-11'>
-        <h1 className='text-gray-900 fw-bolder mb-3'>Sign In</h1>
-        <div className='text-gray-500 fw-semibold fs-6'>Your Social Campaigns</div>
-      </div>
-      {/* begin::Heading */}
+        <h1 className='text-gray-900 fw-bolder mb-3'>
+          DNS Management System
+        </h1>
 
-      {/* begin::Login options */}
-      <div className='row g-3 mb-9'>
-        {/* begin::Col */}
-        <div className='col-md-6'>
-          {/* begin::Google link */}
-          <a
-            href='#'
-            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
-          >
-            <img
-              alt='Logo'
-              src={toAbsoluteUrl('media/svg/brand-logos/google-icon.svg')}
-              className='h-15px me-3'
-            />
-            Sign in with Google
-          </a>
-          {/* end::Google link */}
+        <div className='text-gray-500 fw-semibold fs-6'>
+          Sign in to continue
         </div>
-        {/* end::Col */}
+      </div> 
 
-        {/* begin::Col */}
-        <div className='col-md-6'>
-          {/* begin::Google link */}
-          <a
-            href='#'
-            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
-          >
-            <img
-              alt='Logo'
-              src={toAbsoluteUrl('media/svg/brand-logos/apple-black.svg')}
-              className='theme-light-show h-15px me-3'
-            />
-            <img
-              alt='Logo'
-              src={toAbsoluteUrl('media/svg/brand-logos/apple-black-dark.svg')}
-              className='theme-dark-show h-15px me-3'
-            />
-            Sign in with Apple
-          </a>
-          {/* end::Google link */}
-        </div>
-        {/* end::Col */}
-      </div>
-      {/* end::Login options */}
-
-      {/* begin::Separator */}
-      <div className='separator separator-content my-14'>
-        <span className='w-125px text-gray-500 fw-semibold fs-7'>Or with email</span>
-      </div>
-      {/* end::Separator */}
-
-      {formik.status ? (
-        <div className='mb-lg-15 alert alert-danger'>
-          <div className='alert-text font-weight-bold'>{formik.status}</div>
-        </div>
-      ) : (
-        <div className='mb-10 bg-light-info p-8 rounded'>
-          <div className='text-info'>
-            Use account <strong>admin@demo.com</strong> and password <strong>demo</strong> to
-            continue.
-          </div>
+      {/* Error Alert */}
+      {formik.status && (
+        <div className='mb-lg-10 alert alert-danger'>
+          <div className='alert-text fw-bold'>{formik.status}</div>
         </div>
       )}
 
-      {/* begin::Form group */}
+      {/* Email */}
       <div className='fv-row mb-8'>
-        <label className='form-label fs-6 fw-bolder text-gray-900'>Email</label>
+        <label className='form-label fs-6 fw-bolder text-gray-900'>
+          Email
+        </label>
+
         <input
-          placeholder='Email'
+          placeholder='Enter your email'
+          type='email'
+          autoComplete='off'
           {...formik.getFieldProps('email')}
           className={clsx(
             'form-control bg-transparent',
-            {'is-invalid': formik.touched.email && formik.errors.email},
+            {
+              'is-invalid': formik.touched.email && formik.errors.email,
+            },
             {
               'is-valid': formik.touched.email && !formik.errors.email,
             }
           )}
-          type='email'
-          name='email'
-          autoComplete='off'
         />
+
         {formik.touched.email && formik.errors.email && (
           <div className='fv-plugins-message-container'>
             <span role='alert'>{formik.errors.email}</span>
           </div>
         )}
       </div>
-      {/* end::Form group */}
 
-      {/* begin::Form group */}
+      {/* Password */}
       <div className='fv-row mb-3'>
-        <label className='form-label fw-bolder text-gray-900 fs-6 mb-0'>Password</label>
+        <label className='form-label fw-bolder text-gray-900 fs-6 mb-0'>
+          Password
+        </label>
+
         <input
           type='password'
           autoComplete='off'
+          placeholder='Enter your password'
           {...formik.getFieldProps('password')}
           className={clsx(
             'form-control bg-transparent',
             {
-              'is-invalid': formik.touched.password && formik.errors.password,
+              'is-invalid':
+                formik.touched.password && formik.errors.password,
             },
             {
-              'is-valid': formik.touched.password && !formik.errors.password,
+              'is-valid':
+                formik.touched.password && !formik.errors.password,
             }
           )}
         />
+
         {formik.touched.password && formik.errors.password && (
           <div className='fv-plugins-message-container'>
             <div className='fv-help-block'>
@@ -195,21 +150,17 @@ export function Login() {
           </div>
         )}
       </div>
-      {/* end::Form group */}
 
-      {/* begin::Wrapper */}
-      <div className='d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8'>
+      {/* Forgot Password */}
+      {/* <div className='d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8'>
         <div />
 
-        {/* begin::Link */}
         <Link to='/auth/forgot-password' className='link-primary'>
-          Forgot Password ?
+          Forgot Password?
         </Link>
-        {/* end::Link */}
-      </div>
-      {/* end::Wrapper */}
+      </div> */}
 
-      {/* begin::Action */}
+      {/* Submit */}
       <div className='d-grid mb-10'>
         <button
           type='submit'
@@ -217,19 +168,23 @@ export function Login() {
           className='btn btn-primary'
           disabled={formik.isSubmitting || !formik.isValid}
         >
-          {!loading && <span className='indicator-label'>Continue</span>}
+          {!loading && (
+            <span className='indicator-label'>Sign In</span>
+          )}
+
           {loading && (
-            <span className='indicator-progress' style={{display: 'block'}}>
+            <span
+              className='indicator-progress'
+              style={{display: 'block'}}
+            >
               Please wait...
               <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
             </span>
           )}
         </button>
       </div>
-      {/* end::Action */}
-
       <div className='text-gray-500 text-center fw-semibold fs-6'>
-        Not a Member yet?{' '}
+        Don’t have an account?{' '}
         <Link to='/auth/registration' className='link-primary'>
           Sign up
         </Link>
